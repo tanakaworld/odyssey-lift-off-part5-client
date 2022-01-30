238361ca0555243b61212b969e214a5d9b2a6327
tanakaworld <yutaro.tanaka.world@gmail.com>
2022-01-30 20:46:21 +0900

    update server url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://catstronaut-b28bce2d1437245417.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
